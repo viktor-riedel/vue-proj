726198f87dd7ea76813644dd0bef9be057fb2d9c
Viktor Riedel <viktor.riedel@amlhub.co.nz>
2023-08-02 09:41:35 +1200

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-    <test-component>
-        There is a test
-    </test-component>
 </template>
 
 <script setup>
-    import TestComponent from "@/components/TestComponent.vue";
 </script>
 
 <style scoped>
